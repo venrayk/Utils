563562d83de7bb0daa64287b3b79877324e8be57
venrayk <venrayk@163.com>
2017-03-03 16:33:59 +0800

    add copyObject method

--- Util.js
+++ Util.js
@@ -607,3 +607,15 @@ Utils.prototype.changeObj=function (listOrItem) {
 
     return flag;
 },
+
+
+/**
+ * 复制对象
+ */
+Utils.prototype.copyObject=function(source){
+    var result={};
+    for (var key in source) {
+      result[key] = typeof source[key]==='object'? deepCoyp(source[key]): source[key];
+    }
+    return result;
+}
