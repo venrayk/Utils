7f427e859d249a08edd03c8b3eddbf358c892dbd
venrayk <venrayk@163.com>
2017-10-27 10:31:21 +0800

    Update Util.js

--- Util.js
+++ Util.js
@@ -619,3 +619,100 @@ Utils.prototype.copyObject=function(source){
     }
     return result;
 }
+
+
+//加法
+Utils.prototype.numberAdd=function (arg1, arg2) {
+        if (!arg1) { arg1 = 0; }
+        if (!arg2) { arg2 = 0; }
+        var r1, r2, m;
+
+        try {
+          r1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+          r1=0;
+        }
+        try {
+          r2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+          r2=0;
+        }
+
+        m = Math.pow(10, Math.max(r1,r2));
+
+        var arg1Mul = utilsObj.numberMul(arg1, m);
+        var arg2Mul = utilsObj.numberMul(arg2, m);
+
+        return (arg1Mul + arg2Mul) / m ;
+      },
+ //减法
+Utils.prototype.numberSub=function (arg1,arg2) {
+        if (!arg1) { arg1 = 0; }
+        if (!arg2) { arg2 = 0; }
+
+        try {
+          r1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+          r1=0;
+        }
+        try {
+          r2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+          r2=0;
+        }
+
+        m = Math.pow(10, Math.max(r1,r2));
+        n = (r1 >= r2) ? r1 : r2;
+
+        var arg1Mul = utilsObj.numberMul(arg1, m);
+        var arg2Mul = utilsObj.numberMul(arg2, m);
+
+        return ((arg1Mul - arg2Mul) / m).toFixed(n);
+      }
+//乘法
+Utils.prototype.numberMul=function (arg1, arg2) {
+        if (!arg1) { arg1 = 0; }
+        if (!arg2) { arg2 = 0; }
+
+        var m = 0,
+            s1 = arg1.toString(),
+            s2=arg2.toString();
+
+        try{
+          m += s1.split(".")[1].length;
+        } catch (e) {}
+        try{
+          m += s2.split(".")[1].length;
+        } catch (e) {}
+
+        var tmp = Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+
+        return tmp;
+       };
+//除法
+Utils.prototype.numberDiv=function (arg1, arg2){
+        if(!arg1 || !arg2) { return 0; }
+
+        var t1 = 0, t2 = 0, r1, r2;
+
+        try{
+          t1 = arg1.toString().split(".")[1].length;
+        } catch(e){}
+        try{
+          t2=arg2.toString().split(".")[1].length;
+        }catch(e){}
+
+        r1 = Number(arg1.toString().replace(".",""));
+        r2 = Number(arg2.toString().replace(".",""));
+
+        return utilsObj.numberMul((r1/r2), Math.pow(10,t2-t1));
+
+        // with(Math){
+        //   r1=Number(arg1.toString().replace(".",""));
+        //   r2=Number(arg2.toString().replace(".",""));
+        //   var tmp= utilsObj.numberMul((r1/r2),pow(10,t2-t1));
+        //
+        //   return tmp;
+        // }
+      }
+
